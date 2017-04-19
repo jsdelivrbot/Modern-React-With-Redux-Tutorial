d038514344aea7127e9be25158ed10ee45e8ee7c
Risa Fletcher <risafletcher@Risas-MacBook-Pro.local>
2017-04-19 16:43:56 -0700

    search bar, css styling

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyDEILnJbI1sDVRN3kbKSVsUOtDPT-re2WA';
 
@@ -11,19 +12,32 @@ class App extends Component {
     constructor(props) {
         super(props);
 
-        this.state = { videos: [] };
+        this.state = { 
+            videos: [],
+            selectedVideo: null
+        };
 
-        YTSearch({ key: API_KEY, term: 'surfboards' }, videos => {
-            this.setState({ videos });
-            // ES6 shorthand for this.setState({videos: videos})
-        })
+        this.videoSearch('cats');
+    }
+
+    videoSearch(term) {
+        YTSearch({ key: API_KEY, term: term }, videos => {
+            this.setState({ 
+                videos: videos,
+                selectedVideo: videos[0]
+            });
+        });
+
     }
 
     render() { 
         return (
             <div>
-                <SearchBar />
-                <VideoList videos={this.state.videos} />
+                <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList 
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                    videos={this.state.videos} />
             </div>
         );
     }
